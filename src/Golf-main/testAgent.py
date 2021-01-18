55d08cdd8db12cb9fb7581f54298c1dcfc1290e0
Stone Tao <stonezt2019@gmail.com>
2021-01-18 13:58:32 -0800

    test agents correctly

--- src/Golf-main/testAgent.py
+++ src/Golf-main/testAgent.py
@@ -1,18 +1,33 @@
-# test agent
 import random
-import gym
-import Golf
+try:
+    import gym
+    import Golf
+    from agent import Agent
+except Exception as e:
+    print("ERROR: Missing Agent Class:\n" + str(e))
+    exit()
+
 
 env = gym.make('Golf-v0')
-
+try:
+    urAgent = Agent()
+except Exception as e:
+    print("ERROR: Could not instantiate agent:\n" + str(e))
+    exit()
 total_reward = 0
-for i in range(5):
+episodes = 5
+
+for i in range(episodes):
     episode_reward = 0
-    env.reset()
+    observation = env.reset()
     for t in range(10):
 
         # agent should determine action based on observation
-        action = env.action_space.sample()
+        try:
+            action = urAgent.step(observation)
+        except Exception as e:
+            print("ERROR: Error: Failed to step with agent:\n" + str(e))
+            exit()
         observation, reward, done, info = env.step(action)
         print("Observations: {} ".format(observation))
         print("Reward: {} ".format(reward))
@@ -20,12 +35,14 @@ for i in range(5):
         
         episode_reward += reward
         if done:
+            env.render()
+            env.close()
             print("Episode finished after {} timesteps".format(t+1))
             break
     total_reward += episode_reward
 
-print("Simulation finished with reward: {} ".format(total_reward/5))
+print("Simulation finished with reward: {} ".format(total_reward/episodes))
 print("RESULT")
-print(total_reward/5)
+print(total_reward/episodes)
 
     
